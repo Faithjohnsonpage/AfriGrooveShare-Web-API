ed9dd3023a2d9753fd79de6d61291d666bd7546f
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-09-08 11:41:35 +0100

    Added my album model

---
+++ models/album.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Album class
+"""
+from sqlalchemy import Column, String, Date, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+from typing import List, Dict, Any
+
+
+class Album(BaseModel, Base):
+    """Representation of an Album class"""
+
+    __tablename__ = 'Albums'
+ 
+    title = Column(String(255), nullable=False)
+    artist_id = Column(String(60), ForeignKey('Artists.id'), nullable=False)
+    release_date = Column(Date)
+    cover_image_url = Column(Text)
+ 
+    def __init__(self, *args: List[Any], **kwargs: Dict[str, Any]) -> None:
+        """Initializes Album"""
+        super().__init__(*args, **kwargs)
